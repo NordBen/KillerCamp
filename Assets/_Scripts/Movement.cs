33e41224b4eb58a5107c5a64455a6d21c03ba821
mbl <mikaellaegran@gmail.com>
2025-09-15 15:21:23 +0200

    Fixed movement

--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -6,13 +6,27 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        // float horizontal = Input.GetAxis("Horizontal");
+        // float vertical = Input.GetAxis("Vertical");
+
+        // Vector3 movement = new Vector3(horizontal, vertical, 0.0f);
+
+        // Vector3 newPosition = transform.position + movement.normalized * movementSpeed * Time.deltaTime;
+
+
+
+        // this.transform.position = newPosition;
+        
+        float horizontal = Input.GetAxisRaw("Horizontal"); // instant input
+        float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 movement = new Vector3(horizontal, vertical, 0.0f);
 
-        Vector3 newPosition = transform.position + movement.normalized * movementSpeed * Time.deltaTime;
+        if (movement != Vector3.zero) // only normalize when moving
+        {
+            movement = movement.normalized;
+        }
 
-        this.transform.position = newPosition;
+        transform.position += movement * movementSpeed * Time.deltaTime;
     }
 }
