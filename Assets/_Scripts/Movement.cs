966b9495fcb352feb0d388e442bd9e2d33a01532
NordBen <bebru0048@student.nord.no>
2025-09-17 14:59:16 +0200

    Netcode for gameobjects

--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
+using Unity.Netcode;
 
-public class Movement : MonoBehaviour
+public class Movement : NetworkBehaviour
 {
     [SerializeField] private float movementSpeed = 5.0f;
     // Update is called once per frame
---
+++ Assets/_Scripts/PlayerState.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerState : NetworkBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
