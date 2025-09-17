80bb06f2f5f07853fb6c70b38d7e6fff6e8f3ae7
NordBen <bebru0048@student.nord.no>
2025-09-17 15:41:20 +0200

    Multiplayer setup

---
+++ Assets/_Scripts/ServerController.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ServerController : MonoBehaviour
+{
+    [SerializeField]
+    TMP_InputField code;
+    //[SerializeField]
+    //RelayManager relayManager;
+
+    public async void StartHost()
+    {
+        //await relayManager.CreateRelay(true);
+        SceneManager.LoadSceneAsync("Camp");
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded_Host;
+    }
+
+    private void SceneManager_sceneLoaded_Host(Scene arg0, LoadSceneMode arg1)
+    {
+        NetworkManager.Singleton.StartHost();
+        SceneManager.sceneLoaded -= SceneManager_sceneLoaded_Host;
+    }
+
+    public async void StartClient()
+    {
+        //await relayManager.JoinRelay(code.text);
+        NetworkManager.Singleton.StartClient();
+    }
+
+    public async void StartServer()
+    {
+        //await relayManager.CreateRelay(false);
+        SceneManager.LoadScene("Camp");
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded_Server;
+    }
+
+    private void SceneManager_sceneLoaded_Server(Scene arg0, LoadSceneMode arg1)
+    {
+        NetworkManager.Singleton.StartServer();
+        SceneManager.sceneLoaded -= SceneManager_sceneLoaded_Server;
+    }
+}
