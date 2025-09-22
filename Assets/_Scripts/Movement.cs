82377fd27fe68382918b26de4b4042f0fbc1c759
Myre36 <halldorb04@gmail.com>
2025-09-22 10:19:42 +0200

    Updated level

--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -6,18 +6,7 @@ public class Movement : NetworkBehaviour
     [SerializeField] private float movementSpeed = 5.0f;
     // Update is called once per frame
     private void Update()
-    {
-        // float horizontal = Input.GetAxis("Horizontal");
-        // float vertical = Input.GetAxis("Vertical");
-
-        // Vector3 movement = new Vector3(horizontal, vertical, 0.0f);
-
-        // Vector3 newPosition = transform.position + movement.normalized * movementSpeed * Time.deltaTime;
-
-
-
-        // this.transform.position = newPosition;
-        
+    {        
         float horizontal = Input.GetAxisRaw("Horizontal"); // instant input
         float vertical = Input.GetAxisRaw("Vertical");
 
