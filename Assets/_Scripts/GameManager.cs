a97dbcbfe5b11b320582aaed32399fe15acd6d1e
Myre36 <halldorb04@gmail.com>
2025-09-22 13:49:55 +0200

    Added gameManager

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using Unity.Netcode;
+using Unity.Collections;
+using System.Collections.Generic;
+using System;
+
+public class GameManager : NetworkBehaviour
+{
+    public string playerName;
+
+    public NetworkList<FixedString32Bytes> players = new NetworkList<FixedString32Bytes>();
+    public Dictionary<ulong, FixedString32Bytes> playersDict;
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            playersDict = new Dictionary<ulong, FixedString32Bytes>();
+            NetworkManager.Singleton.OnClientConnectedCallback += Singleton_OnClientConnectedCallback;
+            base.OnNetworkSpawn();
+        }
+        if (IsOwner && IsClient) 
+        {
+            SetPlayerNameRpc(playerName);
+        }
+        
+    }
+
+    [Rpc(SendTo.Server)]
+    private void SetPlayerNameRpc(FixedString32Bytes playerName, RpcParams rpcParams = default)
+    {
+        ulong clientId = rpcParams.Receive.SenderClientId;
+        playersDict[clientId] = playerName;
+        UpdateList();
+    }
+
+    private void Singleton_OnClientConnectedCallback(ulong obj)
+    {
+        playersDict.Add(obj, new FixedString32Bytes());
+        UpdateList();
+    }
+
+    private void UpdateList()
+    {
+        players.Clear();
+        foreach (var kv in playersDict)
+        {
+            players.Add(kv.Value);
+        }
+    }
+}
