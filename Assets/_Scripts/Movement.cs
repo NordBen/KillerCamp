0fb2a5486677fad7f4fac77a8677487495a207bb
Myre36 <halldorb04@gmail.com>
2025-09-22 15:02:10 +0200

    ljbkfad

--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -50,7 +50,7 @@ public class Movement : NetworkBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
-        anticipatedTrans.AnticipateMove(rb.position);
+        anticipatedTrans.AnticipateMove(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        //rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
     }
 }
