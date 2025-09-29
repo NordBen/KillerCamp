60e49d5c08cdbbe4d2fc9c8e6c81b9e9dddee5d3
Myre36 <halldorb04@gmail.com>
2025-09-29 13:32:42 +0200

    Car parts stuff

--- Assets/_Scripts/CarParts.cs
+++ Assets/_Scripts/CarParts.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 
-public class CarParts : MonoBehaviour
+public class CarParts : MonoBehaviour 
 {
+    private bool inRange;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.CompareTag("Player"))
+        {
+            inRange = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            inRange = false;
+        }
     }
 }
