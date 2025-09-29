82ccbfc22f89031aef0bf5adf7ad38b9829c03ea
Myre36 <halldorb04@gmail.com>
2025-09-29 15:01:36 +0200

    THing for us

--- Assets/_Scripts/CarParts.cs
+++ Assets/_Scripts/CarParts.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
 
-public class CarParts : MonoBehaviour 
+public class CarParts : MonoBehaviour, IInteract
 {
     private bool inRange;
+    private GameObject currentPlayer;
+
+    public bool CanInteract()
+    {
+        return inRange;
+    }
+
+    public void Interact()
+    {
+        
+    }
+
+    public bool IsInteracting()
+    {
+        return inRange;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
             inRange = true;
+            currentPlayer = other.gameObject;
         }
     }
 
@@ -17,6 +34,13 @@ public class CarParts : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             inRange = false;
+            currentPlayer = null;
         }
+    }
+
+    private void PickUp()
+    {
+        //currentPlayer.make thing happen
+        GetComponent<Renderer>().enabled = false;
     }
 }
