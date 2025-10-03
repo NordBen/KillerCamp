dd283a0f5569c9e0120d22c249a4cbbb595312fb
Humar <hilmirsg@gmail.com>
2025-10-03 11:26:30 +0200

    maybe?

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -70,6 +70,7 @@ public class GameManager : NetworkBehaviour
         OnGameStarted?.Invoke();
         foreach (var playerId in players)
         {
+            Debug.Log("[ServerStartGame] has been called!" + playerId);
             GameObject player = NetworkManager.Singleton.ConnectedClients[rpcParams.Receive.SenderClientId].PlayerObject.gameObject;
             Vector3 randomPosition = new Vector3 (UnityEngine.Random.Range(MinX, MaxX), UnityEngine.Random.Range(MinY, MaxY), UnityEngine.Random.Range(MinZ, MaxZ));
             player.transform.position = randomPosition;
